2d3852503f76430a362516a0e68b70229b27dcee
SureshKumarDachepalli <sureshkumar.d467@gmail.com>
2024-02-12 16:31:45 +0530

    push .net code

---
+++ src/main/java/dotnet/xy.java
@@ -0,0 +1,10 @@
+package dotnet;
+
+public class xy {
+
+	public static void main(String[] args) {
+		System.out.println("i have to create one class xy ");
+
+	}
+
+}
---
+++ src/test/java/dotnet/yz.java
@@ -0,0 +1,10 @@
+package dotnet;
+
+public class yz {
+
+	public static void main(String[] args) {
+		System.out.println("i have create another class YZ");
+
+	}
+
+}
